b0ed383fd0bf19ada5d3be4f1718359e75649998
Vasyl-Don <chugunnabatareya@gmail.com>
2023-12-04 17:06:50 +0200

    Trnaslating in ukrainian

--- 2/data.js
+++ 2/data.js
@@ -1,26 +1,26 @@
 function validateForm(form) {
     if (!/^[А-Яа-яІіЇїЄєҐґ\s]{3,} [А-ЯІЇЄҐ]{1}. [А-ЯІЇЄҐ]{1}.$/.test(form.pib.value)) {
-        alert('Invalid Full Name');
+        alert('Неправильне ім\'я');
         return false;
     }
 
     if (!/^[А-Яа-яІіЇїЄєҐґ]{2}-\d{2}$/.test(form.group.value)) {
-        alert('Invalid Group format');
+        alert('Неправильний формат групи');
         return false;
     }
 
     if (!/^[А-ЯІЇЄҐ]{2} \d{6}$/.test(form.idCard.value)) {
-        alert('Invalid ID-card format');
+        alert('Неправильний формат ідентифікаційної картки');
         return false;
     }
 
     if (!/^\d{2}\.\d{2}\.\d{4}$/.test(form.dob.value)) {
-        alert('Invalid Date of Birth format');
+        alert('Неправильний формат дати народження');
         return false;
     }
 
     if (!/^\w+[-_.]*\w+@\w+-?\w+\.[a-z]{2,4}$/.test(form.email.value)) {
-        alert("Invalid E-Mail format");
+        alert('Неправильний формат електронної пошти');
         return false;
     }
 
